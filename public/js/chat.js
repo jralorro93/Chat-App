f6cc0164744df366ffafb2a7bc80a161d6a83c68
github username <github email address>
2019-09-09 21:09:26 -0400

    added disable

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,26 +1,44 @@
 const socket = io()
 
+//Elements - Allows manipulation
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+const $locationButton = document.querySelector('#sendLocation')
+
+
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('form#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
+
     socket.emit('sendMessage', message, (error) => {
-      if (error) {
+      $messageFormButton.removeAttribute('disabled')
+      $messageFormInput.value = ''
+      $messageFormInput.focus()
+      
+        if (error) {
           return console.log(error)
       }
-      
+
         console.log('The message was delivered')
     })
 })
 
-document.querySelector('button#sendLocation').addEventListener('click', () => {
+$locationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
+
+    $locationButton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position)=> {
         socket.emit('sendLocation', {
@@ -28,6 +46,7 @@ document.querySelector('button#sendLocation').addEventListener('click', () => {
             long: position.coords.longitude
         }, (message) => {
             console.log(message)
+            $locationButton.removeAttribute('disabled')
         })
     })
 })
